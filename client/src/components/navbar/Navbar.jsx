28bf8d5a78769753795781d8a6c327e8d6f40c99
Sahil <sahil.kumar5729@gmail.com>
2024-04-13 01:22:21 +0530

    Add hamburger menu for small screen navigation

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import "./navbar.scss";
 
 export default function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <nav>
       <div className="left">
@@ -15,7 +18,26 @@ export default function Navbar() {
       </div>
       <div className="right">
         <a href="/">Sign In</a>
-        <a href="/" className="sign-up">Sign Up</a>
+        <a href="/" className="sign-up">
+          Sign Up
+        </a>
+        <div className="menu-icon">
+          <img
+            src="menu.png"
+            alt="Menu Icon"
+            onClick={() => setShowMenu((prev) => !prev)}
+          />
+        </div>
+        <div className={showMenu ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/" className="sign-up">
+            Sign Up
+          </a>
+        </div>
       </div>
     </nav>
   );
